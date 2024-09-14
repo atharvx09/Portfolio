e75a7617c62af247222ca43d6be165fd7aa3082b
atharvx09 <113002246+atharvx09@users.noreply.github.com>
2024-09-14 11:43:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/AtharvLogo.png";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
@@ -9,14 +9,15 @@ const navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-          <img className="mx-1 w-10" src={logo} alt="logo" />
+          <img className="mx-1 w-16" src={logo} alt="logo" />
 
         </div>
         <div className="m-6 flex gap-5 px-1 text-2xl text-white">
-            <FaGithub />
-            <FaLinkedin />
-            <FaInstagram />
-            <FaSquareXTwitter/>
+            <a href="https://github.com/atharvx09/"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/atharv-karole/"><FaLinkedin /></a>
+            <a href="https://www.instagram.com/atharvkarole_/"><FaInstagram /></a>
+            <a href="https://x.com/KaroleAtharv"><FaSquareXTwitter/></a>
+            
             
         </div>
     </nav>
