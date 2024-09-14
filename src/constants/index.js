4717088749d2afdc1278f208bb704d475e7e7526
atharvx09 <113002246+atharvx09@users.noreply.github.com>
2024-09-14 11:39:53 +0530

    index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-3.png";
+import project3 from "../assets/projects/project-23.png";
+import project4 from "../assets/projects/project-4.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a strong foundation in modern web technologies. Skilled in crafting intuitive and visually appealing user interfaces. With hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.JS, Express.JS,  and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With hands on experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -27,32 +27,32 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Hopes Alive",
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB","Next.Js","Express"],
   },
   {
     title: "Portfolio Website",
-    image: project3,
+    image: project2,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "Through a Portfolio website showcasing my creative journey through innovative design and impactful projects and experiences.",
+    technologies: ["HTML", "CSS", "JavaScript", "React","Tailwind-Css","Frame-Motion"],
   },
   {
-    title: "Blogging Platform",
+    title: "YUG Travels",
+    image: project3,
+    description:
+      "An ultimate Website for reliable self-drive and taxi services, offering flexibility and comfort for every journey in Bhopal, India.",
+    technologies: ["HTML", "CSS", "React", "JavaScript","Frame_motion"],
+  },
+  {
+    title: "TIC-TAC_TOE",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A classic Tic-Tac-Toe Game using different technologies.",
+    technologies: ["HTML", "CSS", "JavaScript"],
   },
 ];
 
