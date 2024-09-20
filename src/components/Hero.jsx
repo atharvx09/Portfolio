747d17b9231c9bc96119207c112ae693712bd5e7
atharvx09 <113002246+atharvx09@users.noreply.github.com>
2024-09-20 09:40:13 +0530

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/AtharvImage.jpg";
+import profilePic from "../assets/IMG_7345.png";
 import {motion} from "framer-motion";
 
 
